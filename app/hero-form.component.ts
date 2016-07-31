88a5d12966f64ec5cf03fd378200b18ed34fcc68
Carlos <fromthespigot@gmail.com>
2016-07-30 23:20:58 -0400

    Completed Angular Forms Tutorial
    Made a functional form with custom css styling and features.

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -14,6 +14,12 @@ export class HeroFormComponent {
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
   onSubmit() { this.submitted = true; }
+  active = true;
+  newHero() {
+    this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
